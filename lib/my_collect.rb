9790dac6358bf618e90c6a884d4c32fa176d6658
Nadia Lee <miss.nadia.lee@gmail.com>
2018-01-09 21:54:41 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
+def my_collect(array)
+  array = []
+  i = 0
 
+  while i < array.length
+    yield(array[i])
+    i = i + 1
+  end
+  
+  array.each do | element |
+    element.split(" ").first
+    array << element
+  end
+  array
+end
 

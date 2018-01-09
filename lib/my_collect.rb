abdaa74df6e9430aaaa4967c5fd26a6d8ab4d371
Nadia Lee <miss.nadia.lee@gmail.com>
2018-01-09 21:59:36 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,10 @@
 def my_collect(array)
-  array = []
+  new_array = []
   i = 0
 
   while i < array.length
-    array << yield(array[i])
+    new_array << yield(array[i])
     i += 1
   end
-
-  array
+  new_array
 end
